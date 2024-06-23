0071f2c10825f0123082a7657255cfbd6cf278d1
TpFire <tyler101price@gmail.com>
2024-06-23 19:29:29 -0400

    Allows the game to be played in differing browser sizes

--- PingPong/scripts.js
+++ PingPong/scripts.js
@@ -1,23 +1,32 @@
 let canvas = document.querySelector("#canvas");
 let ctx = canvas.getContext("2d");
-
-let width = canvas.width;
-let height = canvas.height;
+let game_width = canvas.width;
+let game_height = canvas.height;
+// Minimum size for the game is 300 x 300. Can be 400 x 400 if available.
+if (game_width < window.innerWidth &&
+    game_height < window.innerHeight) {
+        game_width += 100;
+        game_height += 100;
+    }
+canvas.width = window.innerWidth - 20;
+canvas.height = window.innerHeight - 20;
+const game_xcoord = (canvas.width - game_width) / 2;
+const game_ycoord = (canvas.height - game_height) / 2;
 
 const paddle_width = 10;
 const paddle_height = 50;
-const player_xcoord = 35;
-const ai_xcoord = width - paddle_width - 35;
+const player_xcoord = game_xcoord + 35;
+const ai_xcoord = game_xcoord + game_width - paddle_width - 35;
 
-let player_ycoord = (height / 2) - (paddle_height / 2);
-let ai_ycoord = (height / 2) - (paddle_height / 2);
+let player_ycoord = (canvas.height / 2) - (paddle_height / 2);
+let ai_ycoord = player_ycoord;
 let ai_speed = 8;
 
 let pong_size = 10;
-let pong_xcoord = width / 2 - pong_size / 2;
-let pong_ycoord = height / 2 - pong_size / 2;
-let pong_xspeed = 4;
-let pong_yspeed = 6;
+let pong_xcoord = canvas.width / 2 - pong_size / 2;
+let pong_ycoord = canvas.height / 2 - pong_size / 2;
+let pong_xspeed = 2;
+let pong_yspeed = 4;
 
 let player_score = 0;
 let ai_score = 0;
@@ -54,20 +63,20 @@ function pauseGame() {
 
 function drawBackground() {
     ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, width, height);
+    ctx.fillRect(game_xcoord, game_ycoord, game_width, game_height);
 }
 
 function drawStartScreen() {
     ctx.font = "100px monospace";
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
-    ctx.fillText("PONG", width/2, height/2);
+    ctx.fillText("PONG", canvas.width/2, game_ycoord + game_height/2);
     ctx.font = "22px monospace";
-    ctx.fillText("CLICK TO BEGIN", width/2, height/2 + 50);
+    ctx.fillText("CLICK TO BEGIN", canvas.width/2, game_ycoord + game_height/2 + 50);
     ctx.fill();
 }
 
-function drawPlayer(size=player_height) {
+function drawPlayer(size=paddle_height) {
     ctx.fillStyle = "red";
     ctx.fillRect(player_xcoord, player_ycoord, paddle_width, size);
 }
@@ -78,8 +87,17 @@ function drawAI() {
 }
 
 function drawPong(size=pong_size) {
-    ctx.fillStyle = "white";
-    ctx.fillRect(pong_xcoord, pong_ycoord, size, size);
+    ctx.fillStyle = "yellow";
+    if (pong_xcoord < game_xcoord)
+        ctx.fillRect(game_xcoord, pong_ycoord, size, size);
+    else if (pong_xcoord > game_xcoord + game_width)
+        ctx.fillRect(game_xcoord + game_width - pong_size, pong_ycoord, size, size);
+    else if (pong_ycoord < game_ycoord)
+        ctx.fillRect(pong_xcoord, game_ycoord, size, size);
+    else if (pong_ycoord + size > game_ycoord + game_height)
+        ctx.fillRect(pong_xcoord, game_ycoord + game_height - size, size, size);
+
+    else { ctx.fillRect(pong_xcoord, pong_ycoord, size, size) }; 
 }
 
 // Displays the current score. 
@@ -89,9 +107,9 @@ function drawScore() {
     ctx.font = "30px monospace";
     ctx.fillStyle = "yellow";
     ctx.textAlign = "left";
-    ctx.fillText(player_score.toString(), 50, 50);
+    ctx.fillText(player_score.toString(), game_xcoord + 50, game_ycoord + 50);
     ctx.textAlign = "right";
-    ctx.fillText(ai_score.toString(), width - 50, 50);
+    ctx.fillText(ai_score.toString(), game_width + game_xcoord - 50, game_ycoord + 50);
 }
 
 // Updates position of the ball. Currently, it begins by moving bottom-right. 
@@ -101,7 +119,7 @@ function updateBall() {
 }
 
 // Calls the draw functions and fills them in.
-async function redrawScreen() {
+function redrawScreen() {
     drawBackground();
     drawPlayer(paddle_height);
     drawAI();
@@ -120,20 +138,20 @@ function resetPong() {
 
 // For keyboard presses.
 function movePlayerUp() {
-    if (player_ycoord == 0)
+    if (player_ycoord == game_ycoord)
         return;
     player_ycoord -= 10;
-    if (player_ycoord <= 0)
-        player_ycoord = 0;
+    if (player_ycoord <= game_ycoord)
+        player_ycoord = game_ycoord;
 }
 
 // For keyboard presses.
 function movePlayerDown() {
-    if (player_ycoord + paddle_height == height)
+    if (player_ycoord + paddle_height == game_ycoord + game_height)
         return;
     player_ycoord += 10;
-    if (player_ycoord + paddle_height >= height)
-        player_ycoord = height - paddle_height;
+    if (player_ycoord + paddle_height >= game_ycoord + game_height)
+        player_ycoord = game_ycoord + game_height - paddle_height;
 }
 
 // Moves the AI if the ball is above or below the paddle.
@@ -156,11 +174,10 @@ function moveAI() {
         ai_ycoord += ai_speed;
 
     // Make sure the AI does not go above or below the game.
-    if (ai.top < 0)
-        ai_ycoord = 0;
-    else if (ai.bottom > height)
-        ai_ycoord = height - paddle_height;
-
+    if (ai_ycoord < game_ycoord)
+        ai_ycoord = game_ycoord;
+    else if (ai_ycoord + paddle_height > game_ycoord + game_height)
+        ai_ycoord = game_ycoord + game_height - paddle_height;
 }
 
 // Increases the speed of the pong when condiiton is true
@@ -227,7 +244,7 @@ function checkCollisions() {
     };
 
     // Check left and right of pong and screen.
-    if (pong.left <= 0) {
+    if (pong.left <= game_xcoord) {
         if (++ai_score >= MAX_SCORE) {
             gameOver = true;
         }
@@ -237,7 +254,7 @@ function checkCollisions() {
             resetPong();
         }, 1000);
     }
-    else if (pong.right >= width) {
+    else if (pong.right >= game_xcoord + game_width) {
         if (++player_score >= MAX_SCORE) {
             gameOver = true;
         }
@@ -249,7 +266,7 @@ function checkCollisions() {
 
     }
     // Check top and bottom of pong and screen.
-    else if (pong.top <= 0 || pong.bottom >= height) {
+    else if (pong.top <= game_ycoord || pong.bottom >= game_ycoord + game_height) {
         pong_yspeed *= -1;
     }
     // Check right of player paddle and left of pong.
@@ -287,16 +304,25 @@ function drawGameOver() {
     ctx.fillStyle = "white";
     ctx.textAlign = "center";
     if (ai_score > player_score)
-        ctx.fillText("GAME OVER", width / 2, height / 2);
+        ctx.fillText("GAME OVER", canvas.width / 2, (game_height + game_ycoord) / 2);
     else {
-        ctx.fillText("YOU WIN!", width / 2, height / 2);
+        ctx.fillText("YOU WIN!", canvas.width / 2, (game_height + game_ycoord) / 2);
     }
     ctx.font = "20px monospace";
-    ctx.fillText("Press Here To Continue", width / 2, height / 1.4);
+    ctx.fillText("Press Here To Continue", canvas.width/2, (game_height + game_ycoord) / 1.4);
     ctx.fill();
 }
 
-
+// Sets the appropriate booleans to false and resets scores.
+function restartGame() {
+    pauseGame();
+    gameOver = false;
+    beginGame = false;
+    player_score = 0;
+    ai_score = 0;
+    player_ycoord = reset_ypoint;
+    ai_ycoord = reset_ypoint;
+}
 
 // The main loop of the program.
 function play() {
@@ -334,18 +360,6 @@ document.querySelector("html").addEventListener("keydown", e => {
         pauseGame();
 });
 
-// Sets the appropriate booleans to false and resets scores.
-function restartGame() {
-    pauseGame();
-    gameOver = false;
-    beginGame = false;
-    player_score = 0;
-    ai_score = 0;
-    player_ycoord = reset_ypoint;
-    ai_ycoord = reset_ypoint;
-}
-
-
 // Allows the game to start when clicked
 document.querySelector("html").addEventListener("mousedown", e => {
     if (!beginGame) {
@@ -358,13 +372,11 @@ document.querySelector("html").addEventListener("mousedown", e => {
     if (gameOver) {
         mouse_ycoord = e.y - canvas.offsetTop;
         mouse_xcoord = e.x;
-        if (mouse_xcoord > width / 5.5 && mouse_xcoord < width / 1.15 &&
-            mouse_ycoord > height / 1.5 && mouse_ycoord < height / 1.35) {
+        if (mouse_xcoord > game_xcoord * 1.1 && mouse_xcoord < (game_width + game_xcoord) / 1.05 &&
+            mouse_ycoord > (game_ycoord + game_height) / 1.5 && mouse_ycoord < (game_ycoord + game_height) / 1.35) {
                 restartGame();
                 requestAnimationFrame(play);
             }
-            //ctx.fillText("Press Here To Continue", width / 2, height / 1.4);
-
     }
 });
 
@@ -373,9 +385,9 @@ document.querySelector("html").addEventListener("mousedown", e => {
 document.querySelector("html").addEventListener("mousemove", e => {
     if (!paused) {
         player_ycoord = e.y - canvas.offsetTop;
-        if (player_ycoord < 0)
-            player_ycoord = 0;
-        else if (player_ycoord + paddle_height > height)
-            player_ycoord = height - paddle_height;
+        if (player_ycoord < game_ycoord)
+            player_ycoord = game_ycoord;
+        else if (player_ycoord + paddle_height > game_ycoord + game_height)
+            player_ycoord = game_ycoord + game_height - paddle_height;
     }
 });
